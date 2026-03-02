7ba5ba5ccaa3bf5bb2d25628cf98e31b3c3a1d05
TimiMaliki <malikitimi@gmail.com>
2026-03-02 16:35:49 +0100

    removed: storage from appwrite.ts

--- src/lib/Utils/appwrite.ts
+++ src/lib/Utils/appwrite.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import {Client , TablesDB ,Storage , Databases} from 'appwrite';
+import {Client , TablesDB  , Databases} from 'appwrite';
 
 export const appwriteClient = new Client()
 .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
@@ -10,19 +10,9 @@ export const appwriteClient = new Client()
 
   export  const DB = new TablesDB(appwriteClient);
   export const DatabasesDB = new Databases(appwriteClient);
-export const storage = new Storage(appwriteClient);
 
 
- storage.getFilePreview({
-    bucketId: 'photos',
-    fileId: 'sunset.png',
-    width: 1800,
-    height: 1200,
-    borderWidth: 5,
-    borderColor: 'CDCA30',
-    borderRadius: 15,
-    background: 'FFFFFF'
-});
+
 
 
 
